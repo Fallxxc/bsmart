ede8bb41ffd4eb7fe5e01a55ca7918b08f6034c5
xaralaxarala@gmail.com <xaralaxarala@gmail.com>
2021-09-28 08:40:05 +0000

    Add whitenoise

--- bsmart/settings.py
+++ bsmart/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR, "bsmart/static"]
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
